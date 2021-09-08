5ed23fcd9ae543d4c7f2a3c5b9b744a673119f6b
Shivakshi <83969489+Shivakshi29@users.noreply.github.com>
2021-09-08 12:47:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20 ){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+});
+
+// typing animation - Home page
+var typed = new Typed(".typing", {
+    strings: ["an <span class='titleControl'>Illustrator</span>", "a <span class='titleControl'>Designer</span>", "a <span class='titleControl'>Photographer</span>", "a <span class='titleControl'>Freelancer</span>"], 
+    
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["an <span class='titleControl'>Illustrator</span>", "a <span class='titleControl'>Designer</span>", "a <span class='titleControl'>Photographer</span>", "a <span class='titleControl'>Freelancer</span>"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+// owl carousel script
+$('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items: 2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
